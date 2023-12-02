047cf9d3e6df1429ab60968f3a33cb352b39a154
Nurul Insani Dewi <nurulinsanid@gmail.com>
2023-12-02 08:33:49 +0700

    tweaks auth layout

--- src/components/auth/components/AuthLayout.jsx
+++ src/components/auth/components/AuthLayout.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-
-
-export const AuthLayout = ({children}) => {
+export const AuthLayout = ({ children }) => {
   return (
-    <main className="h-screen grid grid cols-1 lg:grid-cols-2">
-    <div className="hidden lg:block py-10 pr-px-10 items-center bg-zinc-100 ">
-      <section className=" flex justify-center items-centerw-[640px]">
-        <Image 
-        src="/logo.jpg"
-        width={620}
-        height={420}
-        alt="logo"
-        />
+    <main className="h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="hidden lg:block py-10 pr-px-10 items-center bg-zinc-100 ">
+        <section className=" flex justify-center items-center w-[640px] h-full mx-auto">
+          <Image src="/logo.jpg" width={620} height={420} alt="logo" />
         </section>
-        </div>
-     <div className="flex justify-center items-center">
-    <section className="w-[320px]">
-    {children}
-    </section></div>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <section className="w-[320px]">
+          <Link href={"/"} className="text-md underline mb-6 block">
+            Back to Homepage
+          </Link>
+          {children}
+        </section>
+      </div>
     </main>
-  )
-}
+  );
+};
