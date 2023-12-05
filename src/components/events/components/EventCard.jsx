a15fd01dd63de61f4f5354528acdc997b941e851
Nurul Insani Dewi <nurulinsanid@gmail.com>
2023-12-05 23:10:18 +0700

    improve card event

--- src/components/events/components/EventCard.jsx
+++ src/components/events/components/EventCard.jsx
@@ -16,23 +16,23 @@ export const EventCard = ({
 
   return (
     <Link href={`/events/${id}`}>
-      <div className="border border-gray-200 hover:shadow-medium transition-all rounded-xl">
-        <div>
-          <Image
-            src={"/event-thumb.png"} // temporary using static
-            width={700}
-            height={100}
-            alt="Picture of the author"
-            className="rounded-tl-xl rounded-tr-xl object-cover"
-          />
-        </div>
-        <div className="p-5 flex flex-col justify-between">
+      <div className="border border-gray-200 hover:shadow-medium transition-all rounded-xl h-full">
+        <div className="h-full flex flex-col justify-between">
           <div>
-            <h2 className="text-xl font-semibold mb-2">{name}</h2>
-            <div className="text-gray-400">{location}</div>
-            <div className="text-sm">{date}</div>
+            <Image
+              src={"/event-thumb.png"} // temporary using static
+              width={700}
+              height={100}
+              alt="Picture of the event"
+              className="rounded-tl-xl rounded-tr-xl object-cover"
+            />
+            <div className="p-5 pb-0">
+              <h2 className="text-xl font-semibold mb-2">{name}</h2>
+              <div className="text-gray-400">{location}</div>
+              <div className="text-sm">{date}</div>
+            </div>
           </div>
-          <div>
+          <div className="p-5 pt-0">
             <div className="text-xs mt-4 text-yellow-600 pt-4 border-t border-gray-100">
               Participants:
             </div>
