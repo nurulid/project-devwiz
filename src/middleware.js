a6bf4837111c09765acc7c95467657bc20a2322f
Nurul Insani Dewi <nurulinsanid@gmail.com>
2023-12-05 16:35:07 +0700

    feat: jwt - setup middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,27 @@
 import { NextResponse } from "next/server";
+import * as jose from "jose";
 
-export default function middleware(request) {
+export default async function middleware(request) {
+  //inisialisasikan value token ke dalam variable
+  const token = request.cookies.get("token")?.value; //mengambil value token
+  // console.log(token); //cek value token
+
+  //verifikasi token, ada atau tidak
+  if (token) {
     //verifikasi token
-    const cookie = request.cookies.get("token")?.value;
-    if(cookie){
-        return NextResponse.next();
+    try {
+      const secretKey = new TextEncoder().encode(process.env.NEXT_PUBLIC_SECRET_KEY);
+      await jose.jwtVerify(token, secretKey);
+      return NextResponse.next();
+    }
+    catch (error) {
+      //jika error direct ke /login
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+  //jika token tidak ada, direct ke /login
+  return NextResponse.redirect(new URL("/login", request.url));
 }
-    
-    return NextResponse.redirect(new URL("/login", request.url));
-    
-}  
 export const config = {
-    matcher: ["/dashboard/:path*"],
-}
+  matcher: ["/dashboard/:path*"], //route yang di protect
+};
